6ced5ba3ff8866d910164ea5c577a3123a467584
abuzar-4a6c <abuzar_voetbal@msn.com>
2017-10-11 17:02:25 +0200

    dfsf

--- index.php
+++ index.php
@@ -47,7 +47,43 @@
           <span class="glyphicon glyphicon-menu-hamburger"></span>
         </button>
             </div>
-<?php require 'nav_uitloggen.php'; ?>
+            <div id='navbar' class='navbar-collapse collapse'>
+                <ul class='nav navbar-nav'>
+                    <li><a href='index.php'>Home</a></li>
+                    <li class='dropdown'>
+                        <a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>Over ons <span class='caret'></span></a>
+                        <ul class='dropdown-menu'>
+                            <li><a href='ontstaan.php'>Onstaan Discus Club Holland</a></li>
+                            <li><a href='nieuws.php'>Nieuws</a></li>
+                        </ul>
+                    </li>
+                    <li class='dropdown'>
+                        <a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>Forum <span class='caret'></span></a>
+                        <ul class='dropdown-menu'>
+                            <li><a href='actievetopics.php'>Actieve topics</a></li>
+                            <li><a href='nieuwetopics.php'>Nieuw topics</a></li>
+                            <li><a href='#.php'>Favoriete topics</a></li>
+                            <li><a href='#.php'>Ledenlijst</a></li>
+                        </ul>
+                    </li>
+                    <li><a href='wordlid.php'>Word lid!</a></li>
+                    <li class='dropdown'>
+                        <a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>Sponsoren <span class='caret'></span></a>
+                        <ul class='dropdown-menu'>
+                            <li><a href='sponsoren-worden.php'>Ook sponsor worden?</a></li>
+                            <li><a href='sponsoren.php'>Onze sponsoren</a></li>
+                        </ul>
+                    </li>
+                    <li class='dropdown'>
+                        <a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>Albums <span class='caret'></span></a>
+                        <ul class='dropdown-menu'>
+                            <li><a href='#.php'>Upload</a></li>
+                        </ul>
+                    </li>
+                      <li class="navbar-right"><a href="#">Gebruiker</a></li>
+                      <li class="navbar-right"><a href="#">Uitloggen</a></li>
+                </ul>
+            </div>
         </div>
     </nav>
     <div class="container-fluid">
--- verwerk.php
+++ verwerk.php
@@ -2,6 +2,6 @@
     if (isset($_POST['send'])) {
         mail("shadew69@gmail.com","Reactie van DiscusClubHolland",$_POST['bericht']);
 
-        header('Location: http://hierkomteensite.com/');
+        header('Location: /');
     }
 ?>
