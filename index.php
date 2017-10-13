6496c36ca7847fa03283d7575c78982dd93c372c
kaan-4A6C <510507@edu.rocmn.nl>
2017-10-13 14:56:13 +0200

    dinges

--- index.php
+++ index.php
@@ -177,24 +177,28 @@
     </div>
     <footer>
         <div class="footer-wrapper">
-            <div class="col-md-4 text-center">
-                <h3 class="footer-title"><b>Vind ons leuk!</b></h3>
-                <div class="fb-page" data-href="https://www.facebook.com/DiscusClubHolland/" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
-                    <blockquote cite="https://www.facebook.com/DiscusClubHolland/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/facebook">Facebook</a></blockquote>
+            <div class="container text-center">
+                <div class="row">
+                  <div class="col-md-4">
+                      <h3 class="footer-title"><b>Vind ons leuk!</b></h3>
+                      <div class="fb-page" data-href="https://www.facebook.com/DiscusClubHolland/" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+                          <blockquote cite="https://www.facebook.com/DiscusClubHolland/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/facebook">Facebook</a></blockquote>
+                      </div>
+                  </div>
+                  <div class="col-md-4">
+                      <h3 class="footer-title"><b>Volg ons op Twitter!</b></h3>
+                      <a href="https://twitter.com/DiscusHolland?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">Follow @DiscusHolland</a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script><br>
+                      <a class="twitter-timeline col-md-3" href="https://twitter.com/DiscusHolland?ref_src=twsrc%5Etfw"   data-width="380" data-height="480" >Tweets by DiscusHolland</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+                  </div>
+                  <div class="col-md-4">
+                      <h3 class="footer-title"><b>Contact</b></h3>
+                      <form class="" action="verwerk.php" method="post">
+                          <input class="footer-name" type="text" name="naam" value="" placeholder="Naam"><br>
+                          <textarea class="footer-message" name="bericht" placeholder="Uw bericht"></textarea><br>
+                          <input class="lees-meer-btn footer-btn" type="submit" name="send" value="Verzenden">
+                      </form>
+                  </div>
                 </div>
-            </div>
-            <div class="col-md-4 text-center">
-                <h3 class="footer-title"><b>Volg ons op Twitter!</b></h3>
-                <a class="twitter-timeline"  href="https://twitter.com/discusholland?lang=en"  data-widget-id="831416744602431488">Tweets van @HealthBetuwe</a>
-            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-            </div>
-            <div class="col-md-4 col-sm-12 text-center">
-                <h3 class="footer-title"><b>Contact</b></h3>
-                <form class="" action="verwerk.php" method="post">
-                    <input class="footer-name" type="text" name="naam" value="" placeholder="Naam"><br>
-                    <textarea class="footer-message" name="bericht" placeholder="Uw bericht"></textarea><br>
-                    <input class="lees-meer-btn footer-btn" type="submit" name="send" value="Verzenden">
-                </form>
             </div>
             <div class="col-md-12 text-center">
                 <a href="#">&copy; Discus Club Holland</a> | <a href="#">Webdesign door Impact4ROI</a> | <a href="#">Algemene voorwaarden</a>
@@ -206,3 +210,4 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
 </html>
+<!-- https://twitter.com/DiscusHolland -->
