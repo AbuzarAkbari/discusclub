e14543f7938071b21be1dccd18b8777e24899c1e
abuzar-4a6c <abuzar_voetbal@msn.com>
2017-10-18 11:11:21 +0200

    heeey footer is gemaakt

--- footer.php
+++ footer.php
@@ -1,41 +1,41 @@
 <div class="footer-wrapper ">
-    <div class="container-fluid"></div>
-    <div class='container'>
+  <div class="container-fluid"></div>
+     <div class='container'>
         <div class='container-fluid text-center'>
             <div class='row'>
-                <div class='col-md-4'>
-                    <h3 class='footer-title'><b>Vind ons leuk!</b></h3>
+              <div class='col-md-4'>
+                  <h3 class='footer-title'><b>Vind ons leuk!</b></h3>
+                  <br>
+<div class="fb-page" data-href="https://www.facebook.com/DiscusClubHolland/" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/DiscusClubHolland/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/DiscusClubHolland/">Discus Club Holland</a></blockquote></div>
+              </div>
+              <div class='col-md-4'>
+                  <h3 class='footer-title'><b>Volg ons op Twitter!</b></h3>
                     <br>
-                    <div class='fb-page' data-href='https://www.facebook.com/DiscusClubHolland/' data-tabs='timeline' data-small-header='false' data-adapt-container-width='true' data-hide-cover='false' data-show-facepile='true'>
-                        <blockquote cite='https://www.facebook.com/DiscusClubHolland/' class='fb-xfbml-parse-ignore'><a href='https://www.facebook.com/facebook'>Facebook</a></blockquote>
-                    </div>
-                </div>
-                <div class='col-md-4'>
-                    <h3 class='footer-title'><b>Volg ons op Twitter!</b></h3>
+                  <a class='twitter-timeline col-md-3' href='https://twitter.com/DiscusHolland?ref_src=twsrc%5Etfw'   data-width='380' data-height='480' >Tweets by DiscusHolland</a> <script async src='//platform.twitter.com/widgets.js' charset='utf-8'></script>
+              </div>
+              <div class='col-md-4'>
+                  <h3 class='footer-title'><b>Contact</b></h3>
+                  <br>
 
-                    <a href='https://twitter.com/DiscusHolland?ref_src=twsrc%5Etfw' class='twitter-follow-button' data-show-count='false'>Follow @DiscusHolland</a>
-                    <script async src='//platform.twitter.com/widgets.js' charset='utf-8'></script><br>
-                    <a class='twitter-timeline col-md-3' href='https://twitter.com/DiscusHolland?ref_src=twsrc%5Etfw' data-width='380' data-height='495'>Tweets by DiscusHolland</a>
-                    <script async src='//platform.twitter.com/widgets.js' charset='utf-8'></script>
-                </div>
-                <div class='col-md-4'>
-                    <h3 class='footer-title'><b>Contact</b></h3>
-                    <br>
-                    <p class="footer-contact-text">Heeft u feedback of een vraag? <br> Stuur ons een Bericht!</p>
-                    <form class='' action='verwerk.php' method='post'>
-                        <input class='footer-name' type='text' name='naam' value='' placeholder='Naam'><br>
-                        <textarea class='footer-message' name='bericht' placeholder='Uw bericht'></textarea><br>
-                        <input class='lees-meer-btn footer-btn' type='submit' name='send' value='Verzenden'>
-                    </form>
-                    <br>
-                    <p class="footer-contact-text">Adresstraat: Hoogstraat 73552 XJ Utrecht </p>
-                </div>
+                  <form class='' action='verwerk.php' method='post'>
+                      <h5 class="FooterContactTekst">heeft u feedback, of een vraag?
+                          <br>
+                          Stuur ons een bericht!
+                      </h5>
+                      <input class='footer-name' type='text' name='naam' value='' placeholder='Naam'><br>
+                      <textarea class='footer-message' name='bericht' placeholder='Uw bericht'></textarea><br>
+                      <input class='lees-meer-btn footer-btn' type='submit' name='send' value='Verzenden'>
+                      <h5 class="FooterContactTekst"> Adres: Hoogstraat 7 3552 XJ Utrecht </h5>
+                  </form>
+              </div>
             </div>
             <br>
             <div class='col-md-12 text-center'>
-                <a href='#'>&copy; Discus Club Holland</a> | <a href='#'>Webdesign door Impact4ROI</a> | <a href='#'>Algemene voorwaarden</a>
+            <a href='#'>&copy; Discus Club Holland</a> | <a href='#'>Webdesign door Impact4ROI</a> | <a href='#'>Algemene voorwaarden</a>
             </div>
+            <br><br>
         </div>
-    </div>
-    <div class="container-fluid"></div>
-</div>
+      </div>
+        <div class="container-fluid"></div>
+
+  </div>
