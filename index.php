12b1d0aef86e1df711c3d8db3c1d2307919ad2f6
abuzar-4a6c <abuzar_voetbal@msn.com>
2017-10-23 15:24:27 +0200

    nice

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <!-- bootstrap style -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 </head>
 
 <body>
@@ -34,35 +35,46 @@
   <?php
    require 'uitgelogd.php';
     ?>
-<?php require 'slider.php';?>
+  <?php
+   require 'slider.php';
+    ?>
+
     <div class="container">
         <div class="row">
             <br><br>
-            <div class="col-md-12">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                <br><br> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit
-                amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                <br><br> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                <br><br> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit
-                amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                <br><br>
+            <div class="col-md-12 verticalLine">
+                <h1>Welkom bij Discus Club Holland!</h1>
+                <div class="">
+                Categorie: Website | 04-01-2014<br>
+                Beste Lezer,<br>
+<br><br>
+                De ” koning van de Amazone”  heeft Uw aandacht getrokken. U heeft reeds discusvissen in uw  bezit of overweegt deze te gaan aanschaffen, misschien wilt U er op den duur zelfs mee gaan kweken.
+                Het houden van  discusvissen (nakweek) is, als u zich aan de richtlijnen houdt, niet lastig. Voor wildvang vissen gelden trouwens andere eisen en dit is mijns inziens dan ook meer voor de ervaren aquariaan. De discus is een sterke vis die bij goede verzorging behoorlijk oud kan worden. Het houden van deze vissen krijgt een extra dimensie als er in uw aquarium spontaan een koppeltje wordt gevormd dat daadwerkelijk eieren af gaat zetten. Dan slaat bij vele hobbyisten het ”Discusvirus” toe en komt de wens om een kweekpoging te wagen. Voor al deze zaken heeft U kennis nodig die u bij onze vereniging in ruime mate kunt vinden.
+                Maar let wel:  Het dieren welzijn staat op de eerste plaats. U bent immers verantwoordelijk voor Uw dieren, er zijn vele aspecten die een bijdrage aan het welzijn kunnen leveren. Kijk daarom eens goed op deze site en bedenk daarbij :
+<br><br>
+                Discus Club Holland heeft naast honderden leden die veel kennis en ervaring hebben ook diverse specialisten (bijvoorbeeld op het gebied van visziekten) die u kunnen helpen bij het  voorkomen van teleurstellingen in uw hobby.
+<br><br>
+                Voor slechts 25 euro per jaar krijgt u niet alleen toegang tot dit netwerk maar ontvangt u ook 4x per jaar ons full color clubblad en bent u van harte welkom op onze informatieve en bovenal gezellige clubavonden. Ik kan u verzekeren dat het lidmaatschap van Discus Club Holland de beste investering is die u, als u echt goed voor uw vissen wil zorgen, kunt doen. Ik heet u graag welkom bij onze club..
+
+<br><br><br><br>
+
+                Marcel van Hintum
+<br><br>
+                Voorzitter Discus Club Holland
+<br><br>
+</div>
+
             </div>
         </div>
         <br><br>
-    </div>
+
     <div class="row ">
       <div class="conainter-fluid"></div>
       <div class="container">
 
         <div class="col-md-6">
-            <div class="panel panel-primary border-colors">
-                <div class="panel-heading border-color">Reacties</div>
+            <div class="panel panel-primary s">
+                <div class="panel-heading ">Reacties</div>
                 <div class="panel-body">
                     <div class="box">
                         <p class="title-box title-box-color"><b>Lorem slipsum</b></p>
@@ -106,15 +118,20 @@
         <div class="col-md-6">
             <div class="panel panel-primary">
                 <div class="nieuws-box">
-                    <div class="panel-heading border-color">Ander nieuws</div>
-                    <div class="panel-body">
-                        <p><b>lorem slipsum</b></p>
+                    <div class="panel-heading ">Ander nieuws</div>
+                    <div class="panel-body ">
+                        <div class=" col-md-12 verticalLine">
+
+                         <p><b>lorem slipsum</b></p>
                         You think water moves fast? You should see ice. It moves like it has a mind. Like it knows it killed the world once and got a taste for murder. After the avalanche, it took us a week to climb out. Now, I don't know exactly when we turned on each other,
                         but I know that seven of us survived the slide... and only five made it out. Now we took an oath, that I'm breaking now. We said we'd say it was the snow that killed the other two, but it wasn't. Nature is lethal but it doesn't
                         hold a candle to man.
                         <br><br>
                         <button class="lees-meer-btn" type="button" name="button">Lees meer</button>
-                        <br><br>
+
+                </div>
+                        <br><br> <div class="col-md-12"><br><br></div>
+                        <div class=" col-md-12 verticalLine">
                         <p><b>lorem slipsum</b></p>
                         Now that we know who you are, I know who I am. I'm not a mistake! It all makes sense! In a comic, you know how you can tell who the arch-villain's going to be? He's the exact opposite of the hero. And most times they're friends, like you and me! I should've
                         known way back when... You know why, David? Because of the kids. They called me Mr Glass.
@@ -125,11 +142,13 @@
                         <button class="lees-meer-btn" type="button" name="button">Lees meer</button>
                         <br><br>
                     </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
   </div>
+      </div>
     <div class="conainter-fluid"></div>
     <footer>
 <?php require 'footer.php' ; ?>
