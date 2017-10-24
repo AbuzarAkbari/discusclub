1aaf3e8ba35b7cae42148931c4bd5cc16afaed32
abuzar-4a6c <abuzar_voetbal@msn.com>
2017-10-24 11:18:43 +0200

    mnice

---
+++ bericht-abu.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Discusclub Holland</title>
+
+    <!-- custom css -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- font -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+
+    <!-- bootstrap style -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <div id="fb-root"></div>
+    <script>
+    ;(function(d, s, id) {
+  var js,
+    fjs = d.getElementsByTagName(s)[0]
+  if (d.getElementById(id)) return
+  js = d.createElement(s)
+  js.id = id
+  js.src = '//connect.facebook.net/nl_NL/sdk.js#xfbml=1&version=v2.10'
+  fjs.parentNode.insertBefore(js, fjs)
+})(document, 'script', 'facebook-jssdk')
+</script>
+<?php
+ // require 'ingelogd.php';
+  ?>
+  <?php
+   require 'uitgelogd.php';
+    ?>
+  <?php
+   require 'slider.php';
+    ?>
+
+    <div class="container">
+        <div class="row">
+            <br><br>
+            <div class="panel panel-primary ">
+                <div class="panel-heading border-colors"> <h3>Welkom</h3></div>
+                <div class="panel-body padding-padding">
+                    <div class="col-md-12 verticalLine">
+                        <h4>Welkom bij Discus Club Holland!</h4>
+                        <div class="">
+                        Categorie: <a> Website </a> | <span>04-01-2014</span><br>
+                        Beste Lezer,<br>
+                        <br><br>
+                        De ” koning van de Amazone”  heeft Uw aandacht getrokken. U heeft reeds discusvissen in uw  bezit of overweegt deze te gaan aanschaffen, misschien wilt U er op den duur zelfs mee gaan kweken.
+                        Het houden van  discusvissen (nakweek) is, als u zich aan de richtlijnen houdt, niet lastig. Voor wildvang vissen gelden trouwens andere eisen en dit is mijns inziens dan ook meer voor de ervaren aquariaan. De discus is een sterke vis die bij goede verzorging behoorlijk oud kan worden. Het houden van deze vissen krijgt een extra dimensie als er in uw aquarium spontaan een koppeltje wordt gevormd dat daadwerkelijk eieren af gaat zetten. Dan slaat bij vele hobbyisten het ”Discusvirus” toe en komt de wens om een kweekpoging te wagen. Voor al deze zaken heeft U kennis nodig die u bij onze vereniging in ruime mate kunt vinden.
+                        Maar let wel:  Het dieren welzijn staat op de eerste plaats. U bent immers verantwoordelijk voor Uw dieren, er zijn vele aspecten die een bijdrage aan het welzijn kunnen leveren. Kijk daarom eens goed op deze site en bedenk daarbij :
+                        <br><br>
+                        Discus Club Holland heeft naast honderden leden die veel kennis en ervaring hebben ook diverse specialisten (bijvoorbeeld op het gebied van visziekten) die u kunnen helpen bij het  voorkomen van teleurstellingen in uw hobby.
+                        <br><br>
+                        Voor slechts 25 euro per jaar krijgt u niet alleen toegang tot dit netwerk maar ontvangt u ook 4x per jaar ons full color clubblad en bent u van harte welkom op onze informatieve en bovenal gezellige clubavonden. Ik kan u verzekeren dat het lidmaatschap van Discus Club Holland de beste investering is die u, als u echt goed voor uw vissen wil zorgen, kunt doen. Ik heet u graag welkom bij onze club..
+
+                        <br><br><br><br>
+
+                        Marcel van Hintum
+                        <br><br>
+                        Voorzitter Discus Club Holland
+                    <br><br>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <br>
+        </div>
+        </div>
+        <br><br>
+
+    <div class="row ">
+      <div class="conainter-fluid"></div>
+      <div class="container">
+
+          <div class="panel panel-primary ">
+              <div class="panel-heading border-color">Reacties</div>
+              <div class="panel-heading "> #23	Geplaatst door	marco.middelhuis	op zondag 23 februari 2014 10:39:39</div>
+              <div class="panel-body padding-padding">
+                  <div>
+                    het contact formulier werkt niet
+                </div>
+              </div>
+              <div class="panel-heading "> #23	Geplaatst door	marco.middelhuis	op zondag 23 februari 2014 10:39:39</div>
+              <div class="panel-body padding-padding">
+                  <div>
+                    het contact formulier werkt niet
+                </div>
+              </div>
+              <div class="panel-heading "> #23	Geplaatst door	marco.middelhuis	op zondag 23 februari 2014 10:39:39</div>
+              <div class="panel-body padding-padding">
+                  <div>
+                    het contact formulier werkt niet
+                </div>
+              </div>
+          </div>
+          <br>
+          <div class="panel panel-primary ">
+              <div class="panel-heading border-color">Reactie Toevoegen</div>
+                <?php require 'nice.php';?>
+              </div>
+          </div>
+    </div>
+  </div>
+      </div>
+    <div class="conainter-fluid"></div>
+    <footer>
+<?php require 'footer.php' ; ?>
+    </footer>
+
+
+    <!-- bootstrap script -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+</html>
+<!-- https://twitter.com/DiscusHolland -->
---
+++ nice.php
@@ -0,0 +1,14 @@
+
+  <link href="http://netdna.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css" rel="stylesheet">
+  <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
+  <script src="http://netdna.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.js"></script>
+  <link href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.8/summernote.css" rel="stylesheet">
+  <script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.8/summernote.js"></script>
+</head>
+<body>
+  <div id="summernote"><p>Hello Summernote</p></div>
+  <script>
+  $(document).ready(function() {
+    $('#summernote').summernote()
+})
+</script>
