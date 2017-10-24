0cf3e664e63619d4c71c5bae9278d83f76cb2b4d
abuzar-4a6c <abuzar_voetbal@msn.com>
2017-10-24 15:41:19 +0200

    error

--- registeren.php
+++ registeren.php
@@ -57,6 +57,9 @@
                       Als u registreert gaat u akkoord met onze <a href="gebruiksvoorwaarden.php">gebruiksvoorwaarden</a>.<br><br>
 
                       <input type="submit" class="btn btn-primary" name="send" value="Registeren">
+                      <div class="Gelukt">Bent nu geregistreerd</div>
+                      <div class="Error">Error er iets mis gegaan</div>
+
                   </form>
                     <?php
                     if (isset($_POST["send"])) {
