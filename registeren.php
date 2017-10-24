86565743db2f3ac21f4d7fd315eada34df093364
leon in 't Veld <leon3110l@gmail.com>
2017-10-24 15:46:14 +0200

    updated naming

--- registeren.php
+++ registeren.php
@@ -57,8 +57,8 @@
                       Als u registreert gaat u akkoord met onze <a href="gebruiksvoorwaarden.php">gebruiksvoorwaarden</a>.<br><br>
 
                       <input type="submit" class="btn btn-primary" name="send" value="Registeren">
-                      <div class="Gelukt">Bent nu geregistreerd</div>
-                      <div class="Error">Error er iets mis gegaan</div>
+                      <div class="gelukt">Bent nu geregistreerd</div>
+                      <div class="error">Error er iets mis gegaan</div>
 
                   </form>
                     <?php
