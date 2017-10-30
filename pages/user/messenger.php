c180b3b166b3c054ba28836401e3846608787a35
abuzar-4a6c <abuzar_voetbal@msn.com>
2017-10-30 11:52:08 +0100

    message

--- pages/user/messenger.php
+++ pages/user/messenger.php
@@ -119,10 +119,18 @@ require_once("../../includes/tools/security.php");?>
                     <div class="Username"><b>Gebruikersnaam van user</b></div>
                 </div>
                 <div class="imageBackgroundText flexcroll">
-                    <div class="" >
-
+                    <div class="Message">
+                        <div>tekst die ze zelf invullen</div>
                     </div>
-
+                    <div class="Response ">
+                        <div>tekst die ze zelf invullen</div>
+                    </div>
+                    <div class="Message">
+                        <div>tekst die ze zelf invullen</div>
+                    </div>
+                    <div class="Response ">
+                        <div>tekst die ze Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. invullen</div>
+                    </div>
                 </div>
                 <div class="SearchUser">
                     <div class="input-group">
