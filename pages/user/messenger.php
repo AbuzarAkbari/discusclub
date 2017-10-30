fca93f9992a0a885a072fe93d52d1510b322926d
abuzar-4a6c <abuzar_voetbal@msn.com>
2017-10-30 13:07:17 +0100

    message

--- pages/user/messenger.php
+++ pages/user/messenger.php
@@ -91,7 +91,7 @@ require_once("../../includes/tools/security.php");?>
                         <div>tekst die ze zelf invullen</div>
                     </div>
                     <div class="Other">
-                        <div><img src="http://via.placeholder.com/350x150" class="OtherUsers ImageStatic"></div>
+                        <div><img src="/images/profiel/test.jpg" class="OtherUsers ImageStatic"></div>
                         <div class="UsernameTab"><b>Gebruikersnaam</b></div>
                         <div>tekst die ze zelf invullen</div>
                     </div>
@@ -123,10 +123,37 @@ require_once("../../includes/tools/security.php");?>
                         <div>tekst die ze zelf invullen</div>
                     </div>
                     <div class="Response ">
-                        <div>tekst die ze zelf invullen</div>
+                        <div>tekst Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                        Aenean commodo ligula eget dolor.
+                         Aenean massa.
+                         Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                         Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+                         Nulla consequat massa quis enim.
+                         Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
+                         In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
+                         Nullam dictum felis eu pede mollis pretium. Integer tincidunt.
+                         Cras dapibus. Vivamus elementum semper nisi.
+                         Aenean vulputate eleifend tellus. Aenean leo ligula,
+                         porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
+                         Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum.
+                         Aenean imperdiet. Etiam ultricies nisi vel augue.
+                         Curabitur ullamcorper ultricies nisi.
+                         Nam eget dui.die ze zelf invullen
+                         <div><img src="http://chimpmania.com/forum/attachment.php?attachmentid=33425&d=1369770301&thumb=1" class="MessageImage"></div>
+                        </div>
                     </div>
                     <div class="Message">
                         <div>tekst die ze zelf invullen</div>
+                    </div>
+                    <div class="Message">
+                        <div>tekst die Do you see any Teletubbies in here?
+                        Do you see a slender plastic tag clipped to my shirt with my name printed on it?
+                        Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter
+                        that shakes when you put quarters in it? No? Well, that's what you see at a toy store.
+                        And you must think you're in a toy store,
+                        because you're here shopping for an infant named Jeb. zelf invullen
+                        <div><img src="http://via.placeholder.com/350x150" class="MessageImage"></div>
+                    </div>
                     </div>
                     <div class="Response ">
                         <div>tekst die ze Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. invullen</div>
