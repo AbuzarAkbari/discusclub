ce1fd7932c8c0d851e88c793ea13cac8697428a4
abuzar-4a6c <abuzar_voetbal@msn.com>
2017-10-30 14:38:27 +0100

    message

--- pages/user/messenger.php
+++ pages/user/messenger.php
@@ -86,8 +86,28 @@ require_once("../../includes/tools/security.php");?>
                      </p>
                          <div><img src="http://chimpmania.com/forum/attachment.php?attachmentid=33425&d=1369770301&thumb=1" class="messageImage"></div>
                         </div>
+                    <div class="message ">
+                        <p>
+                        tekst Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                        Aenean commodo ligula eget dolor.
+                         Aenean massa.
+                         Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                         Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+                         Nulla consequat massa quis enim.
+                         Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
+                         In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
+                         Nullam dictum felis eu pede mollis pretium. Integer tincidunt.
+                         Cras dapibus. Vivamus elementum semper nisi.
+                         Aenean vulputate eleifend tellus. Aenean leo ligula,
+                         porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
+                         Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum.
+                         Aenean imperdiet. Etiam ultricies nisi vel augue.
+                         Curabitur ullamcorper ultricies nisi.
+                         Nam eget dui.die ze zelf invullen
+                     </p>
+                         <div><img src="http://chimpmania.com/forum/attachment.php?attachmentid=33425&d=1369770301&thumb=1" class="messageImage"></div>
+                        </div>
                     </div>
-
                 <div class="searchUser">
                     <div class="input-group">
                       <input type="text" class="form-control" placeholder="" aria-describedby="basic-addon1">
