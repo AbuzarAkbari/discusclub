7df1c1ce9fe44298ea8e1cd745062979cb2ee62f
abuzar-4a6c <abuzar_voetbal@msn.com>
2017-10-31 16:44:08 +0100

    advertentie

--- pages/about/index.php
+++ pages/about/index.php
@@ -35,7 +35,7 @@
     <br><br>
     <div class="container main">
         <div class="row">
-            <div class="col-md-8">
+            <div class="col-md-12">
                 <div class="panel panel-primary">
                     <div class="panel-heading border-color-blue">Over Discus Club Holland</div>
                     <div class="panel-body padding-padding space">
@@ -52,58 +52,52 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-8"></div>
-
       <div class="col-md-4">
           <div class="panel panel-primary">
               <div class="panel-heading border-colors">Bekijk de nieuwste albums</div>
               <div class="panel-body">
                   <?php
-                      $sth = $dbc->prepare("SELECT * FROM album_reply JOIN image ON image.album_id = album_reply.album_id JOIN album ON album_reply.album_id = album.id  LIMIT 6");
-                      $sth->execute();
-                      $res = $sth->fetchAll(PDO::FETCH_ASSOC);
+                  $sth = $dbc->prepare("SELECT * FROM album_reply JOIN image ON image.album_id = album_reply.album_id JOIN album ON album_reply.album_id = album.id  LIMIT 6");
+                  $sth->execute();
+                  $res = $sth->fetchAll(PDO::FETCH_ASSOC);
 
-                      foreach($res as $key => $value) : ?>
-                        <div class="col-md-4 col-sm-4 ruimte"><a href="/album/<?php echo $value["album_id"]; ?>"></a><img src="<?php echo $value['path']; ?>"></div>
-                      <?php endforeach; ?>
-              </div>
+                  foreach($res as $key => $value) : ?>
+                  <div class="col-md-4 col-sm-4 ruimte"><a href="/album/<?php echo $value["album_id"]; ?>"></a><img src="<?php echo $value['path']; ?>"></div>
+              <?php endforeach; ?>
           </div>
       </div>
-      <div class="col-md-8"> </div>
+          <div class="panel panel-primary">
+              <div class="panel-heading border-colors">Laatste reacties op albums</div>
+              <div class="panel-body">
+                  <?php
+                  $sth = $dbc->prepare("SELECT * FROM album_reply JOIN image ON image.album_id = album_reply.album_id JOIN album ON album_reply.album_id = album.id LIMIT 5");
+                  $sth->execute();
+                  $res = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+                  foreach($res as $key => $value) : ?>
+                  <div class="col-md-4 col-sm-4 laastenieuws"><span class="blauwtxt"><a href="/album/<?php echo $value["album_id"]; ?>"><img src="<?php echo $value['path']; ?>">
+                  </span></a><b><?php echo $value['title'];?></b><br><?php echo $value['created_at']; ?></div>
+              <?php endforeach; ?>
+          </div>
+      </div>
+      </div>
             <div class="col-md-4">
-              <div class="panel panel-primary">
-                <div class="panel-heading border-colors">Laatste reacties op albums</div>
-                <div class="panel-body">
-                    <?php
-                        $sth = $dbc->prepare("SELECT * FROM album_reply JOIN image ON image.album_id = album_reply.album_id JOIN album ON album_reply.album_id = album.id LIMIT 5");
+                <div class="panel panel-primary">
+                    <div class="panel-heading border-colors">Laatste reacties op nieuws</div>
+                    <div class="panel-body">
+                        <?php
+                        $sth = $dbc->prepare("SELECT * FROM news_reply JOIN news ON news_id = news.id LIMIT 5");
                         $sth->execute();
                         $res = $sth->fetchAll(PDO::FETCH_ASSOC);
 
                         foreach($res as $key => $value) : ?>
-                            <div class="col-md-4 col-sm-4 laastenieuws"><span class="blauwtxt"><a href="/album/<?php echo $value["album_id"]; ?>"><img src="<?php echo $value['path']; ?>">
-                            </span></a><b><?php echo $value['title'];?></b><br><?php echo $value['created_at']; ?></div>
-                        <?php endforeach; ?>
-                </div>
-              </div>
-            </div>
-      <div class="col-md-8"> </div>
-      <div class="col-md-4">
-        <div class="panel panel-primary">
-            <div class="panel-heading border-colors">Laatste reacties op nieuws</div>
-            <div class="panel-body">
-                <?php
-                    $sth = $dbc->prepare("SELECT * FROM news_reply JOIN news ON news_id = news.id LIMIT 5");
-                    $sth->execute();
-                    $res = $sth->fetchAll(PDO::FETCH_ASSOC);
-
-                    foreach($res as $key => $value) : ?>
                         <a href="/about/news/<?php echo $value['id']; ?>" class="blauwtxt"><div class="col-md-12 col-sm-12 laastenieuws"><?php echo $value['title'] ?></a><br><?php echo $value['created_at'] ?></div>
                     <?php endforeach; ?>
                 </div>
             </div>
-      </div>
+            </div>
 
-      <div class="col-md-8"> </div>
+
       <div class="col-md-4">
           <div class="panel panel-primary">
               <div class="panel-heading border-colors">Laatste reacties op topics</div>
