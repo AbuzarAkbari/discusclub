3def19a97f3b9cab56f18338694d70e9bb5cd4b7
kaan-4A6C <510507@edu.rocmn.nl>
2017-11-01 15:11:43 +0100

    become pagina word uit db gehaald

--- pages/sponsor/become.php
+++ pages/sponsor/become.php
@@ -113,7 +113,7 @@
           <div class="panel panel-primary">
               <div class="panel-heading border-colors">Advertentie</div>
               <div class="panel-body">
-                  <div class="col-md-12 col-sm-12 ruimte"><img src="http://via.placeholder.com/320x320"> </div>
+                  <div class="col-md-12 col-sm-12 ruimte"><img src="/images/ad/advertentie.jpg"> </div>
               </div>
           </div>
       </div>
@@ -121,12 +121,14 @@
           <div class="panel panel-primary">
               <div class="panel-heading border-colors">Bekijk de nieuwste albums</div>
               <div class="panel-body">
-                  <div class="col-md-4 col-sm-4 ruimte"><img src="http://via.placeholder.com/350x150"> </div>
-                  <div class="col-md-4 col-sm-4 ruimte"><img src="http://via.placeholder.com/350x150"> </div>
-                  <div class="col-md-4 col-sm-4 ruimte"><img src="http://via.placeholder.com/350x150"> </div>
-                  <div class="col-md-4 col-sm-4 ruimte"><img src="http://via.placeholder.com/350x150"> </div>
-                  <div class="col-md-4 col-sm-4 ruimte"><img src="http://via.placeholder.com/350x150"> </div>
-                  <div class="col-md-4 col-sm-4 ruimte"><img src="http://via.placeholder.com/350x150"> </div>
+                  <?php
+                  $sth = $dbc->prepare("SELECT * FROM album_reply JOIN image ON image.album_id = album_reply.album_id JOIN album ON album_reply.album_id = album.id GROUP BY album.id LIMIT 6");
+                  $sth->execute();
+                  $res = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+                  foreach($res as $key => $value) : ?>
+                  <div class="col-md-4 col-sm-4 ruimte"><a href="/album/<?php echo $value["album_id"]; ?>"></a><img src="<?php echo $value['path']; ?>"></div>
+                  <?php endforeach; ?>
               </div>
           </div>
       </div>
@@ -136,11 +138,15 @@
               <div class="panel panel-primary">
                 <div class="panel-heading border-colors">Laatste reacties op albums</div>
                 <div class="panel-body">
-                   <div class="col-md-12 col-sm-12 laastenieuws"><a class="blauwtxt"><span href="https://placeholder.com"><img src="http://via.placeholder.com/50x50"></span></a>Lorem IPsum<br>12-09 23:32</div>
-                   <div class="col-md-12 col-sm-12 laastenieuws"><a class="blauwtxt"><span href="https://placeholder.com"><img src="http://via.placeholder.com/50x50"></span></a>Lorem IPsum<br>12-09 23:32</div>
-                   <div class="col-md-12 col-sm-12 laastenieuws"><a class="blauwtxt"><span href="https://placeholder.com"><img src="http://via.placeholder.com/50x50"></span></a>Lorem IPsum<br>12-09 23:32</div>
-                   <div class="col-md-12 col-sm-12 laastenieuws"><a class="blauwtxt"><span href="https://placeholder.com"><img src="http://via.placeholder.com/50x50"></span></a>Lorem IPsum<br>12-09 23:32</div>
+                    <?php
+                    $sth = $dbc->prepare("SELECT * FROM album_reply JOIN image ON image.album_id = album_reply.album_id JOIN album ON album_reply.album_id = album.id GROUP BY album.id LIMIT 5");
+                    $sth->execute();
+                    $res = $sth->fetchAll(PDO::FETCH_ASSOC);
 
+                    foreach($res as $key => $value) : ?>
+                    <div class="col-md-4 col-sm-4 laastenieuws"><span class="blauwtxt"><a href="/album/<?php echo $value["album_id"]; ?>"><img src="<?php echo $value['path']; ?>">
+                    </span></a><b><?php echo $value['title'];?></b><br><?php echo $value['created_at']; ?></div>
+                <?php endforeach; ?>
                 </div>
               </div>
             </div>
@@ -149,12 +155,14 @@
         <div class="panel panel-primary">
           <div class="panel-heading border-colors">Laatste reacties op nieuws</div>
           <div class="panel-body">
-             <div class="col-md-12 col-sm-12 laastenieuws"><a class="blauwtxt">Lorem ipsum</a><br>12-09 23:32</div>
-             <div class="col-md-12 col-sm-12 laastenieuws"><a class="blauwtxt">Lorem ipsum</a><br>12-09 23:32</div>
-             <div class="col-md-12 col-sm-12 laastenieuws"><a class="blauwtxt">Lorem ipsum</a><br>12-09 23:32</div>
-             <div class="col-md-12 col-sm-12 laastenieuws"><a class="blauwtxt">Lorem ipsum</a><br>12-09 23:32</div>
-             <div class="col-md-12 col-sm-12 laastenieuws"><a class="blauwtxt">Lorem ipsum</a><br>12-09 23:32</div>
-             <div class="col-md-12 col-sm-12 laastenieuws"><a class="blauwtxt">Lorem ipsum</a><br>12-09 23:32</div>
+              <?php
+                  $sth = $dbc->prepare("SELECT * FROM album_reply JOIN image ON image.album_id = album_reply.album_id JOIN album ON album_reply.album_id = album.id GROUP BY album.id LIMIT 5");
+                  $sth->execute();
+                  $res = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+                  foreach($res as $key => $value) : ?>
+                  <a href="/about/news/<?php echo $value['id']; ?>" class="blauwtxt"><div class="col-md-12 col-sm-12 laastenieuws"><?php echo $value['title'] ?></a><br><?php echo $value['created_at'] ?></div>
+                  <?php endforeach; ?>
           </div>
         </div>
       </div>
@@ -164,11 +172,14 @@
         <div class="panel panel-primary">
           <div class="panel-heading border-colors">Laatste reacties op topics</div>
           <div class="panel-body">
-             <div class="col-md-12 col-sm-12 laastenieuws"><a class="blauwtxt">Lorem ipsum</a><br>12-09 23:32</div>
-             <div class="col-md-12 col-sm-12 laastenieuws"><a class="blauwtxt">Lorem ipsum</a><br>12-09 23:32</div>
-             <div class="col-md-12 col-sm-12 laastenieuws"><a class="blauwtxt">Lorem ipsum</a><br>12-09 23:32</div>
-             <div class="col-md-12 col-sm-12 laastenieuws"><a class="blauwtxt">Lorem ipsum</a><br>12-09 23:32</div>
-             <div class="col-md-12 col-sm-12 laastenieuws"><a class="blauwtxt">Lorem ipsum</a><br>12-09 23:32</div>
+              <?php
+                  $sth = $dbc->prepare("SELECT * FROM topic ORDER BY created_at LIMIT 5");
+                  $sth->execute();
+                  $res = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+                  foreach($res as $key => $value) : ?>
+                      <a href="/forum/post/<?php echo $value['id']; ?>" class="blauwtxt"><div class="col-md-12 col-sm-12 laastenieuws"><?php echo $value['title'] ?></a><br><?php echo $value['created_at'] ?></div>
+              <?php endforeach; ?>
           </div>
         </div>
       </div>
