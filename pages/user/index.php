d75216844819efb67d9d80a16378c8f8995415d0
Melvin van Zutphen <melvinvanzutphen1995@outlook.com>
2017-11-01 12:34:51 +0100

    Added user name on user profile

--- pages/user/index.php
+++ pages/user/index.php
@@ -60,7 +60,7 @@ if (isset($_GET["id"])) {
 
                 <div class="col-md-12">
                     <div class="panel panel-primary border-color-blues">
-                        <div class="panel-heading border-color-blue">Informatie</div>
+                        <div class="panel-heading border-color-blue"><?php echo $user_data->first_name.' '.$user_data->last_name; ?></div>
                         <div class="panel-body text-left">
                             <div class="text-center">
                                 <div style="background-image:url('/images/profiel/<?php echo isset($user_data->profile_img) ? $user_data->profile_img : " "; ?>')"; class="img "></div>
