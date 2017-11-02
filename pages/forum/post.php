17fd31a2df4b4733a56aa82c47ffcebd8f7416a2
Melvin van Zutphen <melvinvanzutphen1995@outlook.com>
2017-11-02 16:57:27 +0100

    added some stuff

--- pages/forum/post.php
+++ pages/forum/post.php
@@ -224,10 +224,7 @@ require_once("../../includes/components/nav.php");
                     <div class="panel-body padding-padding ">
                         <div class="wrapper-box col-xs-12">
                             <div class="col-md-2">
-                                <img class="img" src="/images/profiel/<?php echo $row['path']; ?>">
-                                    <br><b>Rol: </b><?php echo $row['name']; ?>
-                                    <br><b>Aantal berichten: <?php echo $x->x_reply; ?></b>
-                                    <br><b>Lid sinds: </b> <?php echo $row['user_created_at']; ?><br><br>
+                                <img src='http://via.placeholder.com/130x130' alt="x">
                             </div>
 
                             <div class="col-md-10">
