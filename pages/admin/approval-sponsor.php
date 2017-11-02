5a31af887fa19e4a6e7e4994bd42f2ee707b8cc3
kaan-4A6C <510507@edu.rocmn.nl>
2017-11-02 13:56:10 +0100

    begonnen met admin sponsor

---
+++ pages/admin/approval-sponsor.php
@@ -0,0 +1,109 @@
+<?php require_once("../../includes/tools/security.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"><link rel="shortcut icon" href="/favicon.ico" />
+    <title>Discusclub Holland</title>
+
+    <!-- custom css -->
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/nieuws.css">
+    <!-- font -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <!-- bootstrap style -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div id="fb-root"></div>
+    <script>
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s);
+            js.id = id;
+            js.src = "//connect.facebook.net/nl_NL/sdk.js#xfbml=1&version=v2.10";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    </script>
+    <?php
+        require_once("../../includes/components/nav.php");
+    ?>
+<br><br>
+    <div class="container main">
+        <div class="row columns">
+            <div class="col-md-12">
+                <div class="panel panel-primary ">
+                    <div class="panel-heading border-colors">Inschrijvingen</div>
+                    <div class="panel-body padding-padding table-responsive">
+                        <table>
+                            <tr>
+                                <th>gebruikersnaam</th>
+                                <th>Ip adres</th>
+                                <th>Iban</th>
+                                <th>Telefoonnummer</th>
+                                <th>Postcode</th>
+                                <th>Adres</th>
+                                <th>Geboortedatum</th>
+                                <th>Stad</th>
+                                <th>Admin tools</th>
+                            </tr>
+                            <?php
+                                $sql = "SELECT *, ip.id, user.id as user_id, user.created_at as user_created_at FROM ip LEFT JOIN user ON ip.user_id = user.id";
+                                $result = $dbc->prepare($sql);
+                                $result->execute();
+                                $rows = $result->fetchAll(PDO::FETCH_ASSOC);
+                            ?>
+                            <?php
+                            foreach ($rows as $ip) :
+                                ?>
+                                <tr>
+                                    <td><a href="/user/<?php echo $ip["user_id"]; ?>"><?php
+                                     echo $ip['first_name'] . " " . $ip['last_name'];
+                                    ?></a></td>
+                                    <td><?php
+                                     echo $ip['ip_address'];
+                                    ?></td>
+                                    <td><?php
+                                     echo isset($ip['user_created_at']) ? $ip['user_created_at'] : $ip['created_at'];
+                                    ?></td>
+                                    <td><?php
+                                     echo $ip['phone'];
+                                    ?></td>
+                                    <td><?php
+                                     echo $ip['postal_code'];
+                                    ?></td>
+                                    <td><?php
+                                     echo $ip['address'],$ip['house_number'];
+                                    ?></td>
+                                    <td><?php
+                                     echo $ip['birthdate'];
+                                    ?></td>
+                                    <td><?php
+                                     echo $ip['city'];
+                                    ?></td>
+                                    <td>
+                                     <a title="Blokeer" href="" type="button" class="btn btn-danger" name="button"><i class="glyphicon glyphicon-remove"></i></a>
+                                     <a title="Deblokeer" href="" type="button" class="btn btn-success" name="button"><i class="glyphicon glyphicon-ok"></i></a></td>
+                                </tr>
+                            <?php
+                            endforeach;
+                            ?>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer>
+<?php require_once("../../includes/components/footer.php") ; ?>
+    </footer>
+    <!-- bootstrap script -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+
+</html>
