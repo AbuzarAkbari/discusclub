f8e300d1fa53d1102cd199e2f51abb7b727c779e
Wesley-4A6B <483509@edu.rocmn.nl>
2017-11-02 11:17:52 +0100

    About page

--- pages/about/index.php
+++ pages/about/index.php
@@ -63,12 +63,12 @@
               <div class="panel-heading border-colors">Bekijk de nieuwste albums</div>
               <div class="panel-body">
                   <?php
-                      $sth = $dbc->prepare("SELECT * FROM album_reply JOIN image ON image.album_id = album_reply.album_id JOIN album ON album_reply.album_id = album.id GROUP BY album.id LIMIT 6");
+                      $sth = $dbc->prepare("SELECT *, album.created_at AS album_created_at FROM album_reply JOIN image ON image.album_id = album_reply.album_id JOIN album ON album_reply.album_id = album.id ORDER BY album_created_at DESC LIMIT 5");
                       $sth->execute();
                       $res = $sth->fetchAll(PDO::FETCH_ASSOC);
 
                       foreach($res as $key => $value) : ?>
-                      <div class="col-md-4 col-sm-4 ruimte"><a href="/album/<?php echo $value["album_id"]; ?>"><img src="/images/album/<?php echo $value['path']; ?>"></div></a>
+                      <div class="col-md-4 col-sm-4 ruimte"><a href="/album/<?php echo $value["album_id"]; ?>"><img src="/images/album/<?php echo $value['path']; ?>"></a><br><?php echo $value['album_created_at'] ?></div>
                       <?php endforeach; ?>
           </div>
       </div>
@@ -76,16 +76,12 @@
               <div class="panel-heading border-colors">Laatste reacties op albums</div>
               <div class="panel-body">
                   <?php
-                  $sth = $dbc->prepare("SELECT * FROM album_reply JOIN image ON image.album_id = album_reply.album_id JOIN album ON album_reply.album_id = album.id GROUP BY album.id LIMIT 5");
+                  $sth = $dbc->prepare("SELECT *, album_reply.created_at AS album_reply_created_at FROM album_reply JOIN album ON album_reply.album_id = album.id ORDER BY album_reply.created_at DESC LIMIT 5");
                   $sth->execute();
                   $res = $sth->fetchAll(PDO::FETCH_ASSOC);
 
                   foreach($res as $key => $value) : ?>
-                  <a href="/album/<?php echo $value["album_id"]; ?>"><div class="col-md-4 col-sm-4 laastenieuws"><span class="blauwtxt"><img src="/images/album/<?php echo $value['path']; ?>">
-                  </span><b><?php echo $value['title'];?></b><br>
-                      <?php echo $value['created_at']; ?>
-                  </div>
-                  </a>
+                  <a href="/album/<?php echo $value['album_id']; ?>" class="blauwtxt"><div class="col-md-12 col-sm-12 laastenieuws"><?php echo $value['title'] ?></a><br><?php echo $value['album_reply_created_at'] ?></div>
                   <?php endforeach; ?>
           </div>
       </div>
@@ -112,7 +108,7 @@
               <div class="panel-heading border-colors">Laatste reacties op topics</div>
               <div class="panel-body">
                   <?php
-                      $sth = $dbc->prepare("SELECT * FROM topic ORDER BY created_at LIMIT 5");
+                      $sth = $dbc->prepare("SELECT * FROM topic ORDER BY created_at DESC LIMIT 5");
                       $sth->execute();
                       $res = $sth->fetchAll(PDO::FETCH_ASSOC);
 
