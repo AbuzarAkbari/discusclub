6bd76445909b39ccbf3fcc5837999749a3276d20
Melvin van Zutphen <melvinvanzutphen1995@outlook.com>
2017-11-03 16:45:22 +0100

    Added todo

--- includes/tools/user-un-favorite.php
+++ includes/tools/user-un-favorite.php
@@ -5,9 +5,9 @@ if($logged_in) {
     $user_id = $_SESSION['user']->id;
     $topic_id = $_GET['id'];
 
-    $sql = "DELETE FROM favorite WHERE user_id = :user_id";
+    $sql = "DELETE FROM favorite WHERE user_id = :user_id AND topic_id = :topic_id";
     $result = $dbc->prepare($sql);
-    $result->execute([":user_id" => $user_id]);
+    $result->execute([":user_id" => $user_id, ":topic_id" => $topic_id]);
 
     header("Location: /forum/post/".$topic_id);
 }
