33b36f5047bf6e414a1d7a854775407ed0d311d6
kaan-4A6C <510507@edu.rocmn.nl>
2017-11-07 15:59:32 +0100

    nieuwe paginas toegevoegd!

--- pages/houdenvan/kweken.php
+++ pages/houdenvan/kweken.php
@@ -34,8 +34,34 @@
     <?php
         require_once("../../includes/components/nav.php");
       ?>
-    <div class="">
-
+    <div class="container main">
+        <br>
+        <div class="row">
+            <h1>Zelf kut vissen kweken?</h1>
+            <hr class="col-md-12">
+            <div class="col-md-6">
+                You think water moves fast? You should see ice. It moves like it has a mind.
+                Like it knows it killed the world once and got a taste for murder. After the avalanche,
+                it took us a week to climb out. Now, I don't know exactly when we turned on each other,
+                but I know that seven of us survived the slide... and only five made it out. Now we took an oath,
+                that I'm breaking now. We said we'd say it was the snow that killed the other two, but it wasn't.
+                Nature is lethal but it doesn't hold a candle to man.<br><br>
+                Like it knows it killed the world once and got a taste for murder. After the avalanche,
+                it took us a week to climb out. Now, I don't know exactly when we turned on each other,
+                but I know that seven of us survived the slide... and only five made it out. Now we took an oath,
+                that I'm breaking now. We said we'd say it was the snow that killed the other two, but it wasn't.
+                Nature is lethal but it doesn't hold a candle to man.<br><br>
+                Like it knows it killed the world once and got a taste for murder. After the avalanche,
+                it took us a week to climb out. Now, I don't know exactly when we turned on each other,
+                but I know that seven of us survived the slide... and only five made it out. Now we took an oath,
+                that I'm breaking now. We said we'd say it was the snow that killed the other two, but it wasn't.
+                Nature is lethal but it doesn't hold a candle to man.
+            </div>
+            <div class="col-md-6">
+                <img src="/images/vis(1).jpg" alt="">
+            </div>
+        </div>
+        <br>
     </div>
     <footer>
         <?php require_once('../../includes/components/footer.php') ; ?>
