f524a1b821b2c628aa80fbc3207bb728b3995c86
Wesley-4A6B <483509@edu.rocmn.nl>
2017-11-10 13:47:32 +0100

    Albums errors

--- includes/tools/album-upload.php
+++ includes/tools/album-upload.php
@@ -1,24 +1,18 @@
 <?php
 if (isset($_POST['upload_album'])) {
     require_once("security.php");
-
-	// echo '<pre>';
-	// print_r($_FILES);
-	// exit;
-
     if ($logged_in) {
     		$album_name = $_POST['album_name'];
 	        $id_poster = $_SESSION['user']->id;
     	if (isset($_FILES['files']) && $_FILES['files']['error'] !== 4) {
+    		$error = '';
 	        $albumsql = "INSERT INTO album (title, user_id, created_at) VALUES (:title, :user_id, NOW())";
             $album_result = $dbc->prepare($albumsql);
-            // var_dump([':title' => $album_name, ':user_id' => $id_poster]);
             $album_result->execute([':title' => $album_name, ':user_id' => $id_poster]);
 			$album_id = $dbc->lastInsertId();
 
 	        for($x = 0; $x  < count($_FILES['files']['name']); $x++) {
 	        	$album_files = $_FILES['files'];
-	        	// var_dump($album_files);
 	            $target_dir = "/images/album/";
 	            $target_file = $target_dir . basename($album_files["name"][$x]);
 	            $uploadOk = 1;
@@ -29,32 +23,33 @@ if (isset($_POST['upload_album'])) {
 	            if ($check !== false) {
 	                $uploadOk = 1;
 	            } else {
-	                echo "File is not an image.";
+	                $error = "File is not an image.";
 	                $uploadOk = 0;
 				}
 
 	            // Check if file already exists
 	            if (file_exists($target_file)) {
-	                echo "Sorry, file already exists.";
+	                $error = "Sorry, geen fotobestand gevonden.";
 	                $uploadOk = 0;
 	            }
 
 	            // Check file size
 	            if ($album_files["size"][$x] > 500000) {
-	                echo "Sorry, your file is too large.";
+	                $error = "Sorry, het bestand is te groot";
 	                $uploadOk = 0;
 	            }
 
 	            // Allow certain file formats
 	            if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 	                && $imageFileType != "gif" ) {
-	                echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+	                 $error = "Sorry, alleen JPG, JPEG, PNG & GIF bestanden zijn toegestaan.";
 	                $uploadOk = 0;
 	            }
 
 	            // Check if $uploadOk is set to 0 by an error
 	            if ($uploadOk == 0) {
-	                echo "Sorry, your file was not uploaded.";
+	                header("Location: /album/upload?error=".$error);
+	                exit();
 
 	                // if everything is ok, try to upload file
 	            } else {
@@ -68,33 +63,19 @@ if (isset($_POST['upload_album'])) {
 	                    '.gif'
 	                ];
 
-	                // foreach ($extensions as $extension) {
-	                //     if (file_exists(_DIR_ . '/' . $target_dir . $_SESSION["user"]->username . $extension)) {
-	                //         unlink(_DIR_ . '/' . $target_dir . $_SESSION["user"]->username . $extension);
-	                //     }
-	                // }
-
-
-
 	                if (move_uploaded_file($album_files["tmp_name"][$x], '../../images'.$path)) {
 	                    $sql = "INSERT INTO image (path, album_id) VALUES (:path, :album_id)";
 	                    $result = $dbc->prepare($sql);
 						$result->execute([':path' => $path, ':album_id' => $album_id]);
 
 					} else {
-	                    echo "Sorry, there was an error uploading your file.";
+	                     $error = "Sorry, er ging iets mis met het uploaden.";
 	                    exit();
 	                }
 	            }
 	        }
 
         }
-
-
-
-        // echo '<pre>';
-        // print_r($_FILES);
-        // exit();
 
         header("Location: /album/post/" . $album_id);
     }
