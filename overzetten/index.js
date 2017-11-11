8a6588d17a58fd7d80c2f2df0babdfc989bd8d1c
leon in 't Veld <leon3110l@gmail.com>
2017-11-11 17:21:00 +0100

    did db overzetten stuff

--- overzetten/index.js
+++ overzetten/index.js
@@ -12,71 +12,96 @@ let data = require('./dhc')
 // map the data to make it easier
 data = data.map(x => ({ name: x.name, data: x.data }))
 
-data.map(x => console.log(x.name))
+const forums = data.filter(x => x.name === "forums")[0].data;
+const users = data.filter(x => x.name === "users")[0].data;
+const forum_topics = data.filter(x => x.name === "forum_topics")[0].data;
+const forum_posts = data.filter(x => x.name === "forum_posts")[0].data;
 
-data.forEach((x, i) => {
-  const total = x.data.length
-  const errorHandler = i => (err, res) => {
-    if (err) {
-      console.log(err)
-    } else {
-      process.stdout.clearLine()
-      process.stdout.cursorTo(0)
-      process.stdout.write(`${i}/${total}`)
-    }
+
+data = null
+
+console.log(forums.length, users.length, forum_topics.length, forum_posts.length)
+
+console.log("data loaded!");
+
+const errorHandler = i => total => (err, res) => {
+  if (err) {
+    console.log(err)
+  } else {
+    process.stdout.clearLine()
+    process.stdout.cursorTo(0)
+    process.stdout.write(`${i}/${total}`)
   }
+}
 
-  if (x.name === 'forums') {
-    x.data.forEach((x, i) => {
-      if (x.is_category == 1) {
-        conn.query(
-          'INSERT INTO category(name, created_at) VALUES(?, NOW())',
-          [x.title],
-          errorHandler,
-        )
-      } else {
-        conn.query(
-          'INSERT INTO sub_category(name, created_at, category_id) VALUES(?, NOW(), ?)',
-          [x.title, x.parent_id],
-          errorHandler(i),
-        )
-      }
-    })
+console.log("inserting users");
+
+users.forEach((x, i) => {
+  let role_id = 2
+  if (x.group_id == 500 || x.group_id == 502 || x.group_id == 400) {
+    role_id = 5
+  } else if (x.group_id == 300) {
+    role_id = 4
   }
-  // {"id":"-1","group_id":"100","username":"Anonymous","password":"","passwordcode":null,"email":"---------","email_authenticated":"1","activationcode":null,"last_login":"2010-08-16 00:26:58","created":"2003-12-09 18:39:56","modified":"2010-08-16 01:04:15","approved":"1","new":"0"}
-  if (x.name === 'users') {
-    x.data.forEach((x, i) => {
-      let role_id = 2
-      if (x.group_id == 500 || x.group_id == 502 || x.group_id == 400) {
-        role_id = 5
-      } else if (x.group_id == 300) {
-        role_id = 4
-      }
+  conn.query(
+    'INSERT INTO user(id, email, first_name, last_name, username, created_at, last_changed, role_id) VALUES(?, ?, ?, ?, ?, ?, ?, ?)',
+    [
+      x.id,
+      x.email,
+      x.username
+        .split(' ')
+        .slice(0, 1)
+        .join('')
+        .trim(),
+      x.username
+        .split(' ')
+        .slice(1)
+        .join(' ')
+        .trim(),
+      x.username,
+      x.created,
+      x.modified,
+      role_id,
+    ],
+    errorHandler(i)(users.length),
+  )
+})
+conn.query(
+  "INSERT INTO user (first_name, last_name, email, username, password, role_id, created_at) VALUES ('john', 'doe', 'john_doe@example.com', 'test', '$2y$10$9UNJC27kiVGmXrn5WUeyPeSktXXF1uTRE2mX8bgOISy2GTLC57pBm', 5, NOW()), ('jane', 'doe', 'jane_doe@example.com', 'bla', '$2y$10$9UNJC27kiVGmXrn5WUeyPeSktXXF1uTRE2mX8bgOISy2GTLC57pBm', 5, NOW());",
+)
 
-      conn.query(
-        'INSERT INTO user(email, first_name, last_name, username, created_at, last_changed, role_id) VALUES(?, ?, ?, ?, ?, ?, ?)',
-        [
-          x.email,
-          x.username
-            .split(' ')
-            .slice(0, 1)
-            .join('')
-            .trim(),
-          x.username
-            .split(' ')
-            .slice(1)
-            .join(' ')
-            .trim(),
-          x.username,
-          x.created,
-          x.modified,
-          role_id,
-        ],
-        errorHandler(i),
-      )
-    })
+console.log("inserting categories")
+
+forums.forEach((x, i) => {
+  if (x.is_category == 1) {
     conn.query(
-      "INSERT INTO user (first_name, last_name, email, username, password, role_id, created_at) VALUES ('john', 'doe', 'john_doe@example.com', 'test', '$2y$10$9UNJC27kiVGmXrn5WUeyPeSktXXF1uTRE2mX8bgOISy2GTLC57pBm', 5, NOW()), ('jane', 'doe', 'jane_doe@example.com', 'bla', '$2y$10$9UNJC27kiVGmXrn5WUeyPeSktXXF1uTRE2mX8bgOISy2GTLC57pBm', 5, NOW());",
+      'INSERT INTO category(id, name, created_at) VALUES(?, ?, NOW())',
+      [x.id, x.title],
+      errorHandler(i)(forums.length),
+    )
+  } else {
+    conn.query(
+      'INSERT INTO sub_category(id, name, created_at, category_id) VALUES(?, ?, NOW(), ?)',
+      [x.id, x.title, x.parent_id],
+      errorHandler(i)(forums.length),
     )
   }
+})
+
+console.log("inserting forum posts")
+
+forum_topics.forEach((x, i) => {
+  let state = 0;
+  if(x.is_sticky) {
+    state = 2;
+  }
+  if(x.is_locked) {
+    state = 1;
+  }
+  const bla = forum_posts.filter(y => y.created == x.created);
+  const content = bla.length > 0 ? bla[0].content_cache : "";
+  // const content = ""
+  // console.log(content)
+  conn.query("INSERT INTO topic(user_id, title, sub_category_id, created_at, last_changed, state_id, content) VALUES (?, ?, ?, ?, ?, ?, ?)",
+            [x.profile_id, x.title, x.forum_id, x.created, x.modified, state, content], errorHandler(i)(forum_topics.length))
 })
