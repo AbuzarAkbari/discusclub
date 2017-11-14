ff9a0bbc10590c5ad4597ea46edc39111f55aca6
Wesley-4A6B <483509@edu.rocmn.nl>
2017-11-14 11:56:08 +0100

    Added html entititititities

--- pages/news/news_post.php
+++ pages/news/news_post.php
@@ -188,7 +188,7 @@ require_once("../../includes/components/nav.php");
                             </div>
 
                             <div class="col-md-9">
-                                 <p><?php echo wordwrap($reply['news_reply_content'], 70, "<br>", true); ?></p>
+                                 <p><?php echo wordwrap(html_entity_decode($reply['news_reply_content']), 70, "<br>", true); ?></p>
                             </div>
 
                 </div>
