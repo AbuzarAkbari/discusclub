d4c6b98a211300264a65e0a2f4b29566539919b8
abuzar-4a6c <abuzar_voetbal@msn.com>
2017-11-14 14:15:17 +0100

    sponsor

--- includes/tools/sponsor-worden.php
+++ includes/tools/sponsor-worden.php
@@ -28,11 +28,9 @@ if(isset($_POST['sponsorverzend'])) {
         $width = $check[0];
         $height = $check[1];
 
-        if ($width > 468 || $height > 60){
-            echo "nice";
-        } else {
-            $error = "Sorry, het bestand is te groot";
-            unlink($sponsor_file["tmp_name"]);
+        if ($width < 480 || $height < 70){
+            $error = "Sorry, het bestand bestaat al.";
+            $uploadOk = 0;
         }
 
         // Check if file already exists
