72aaed62dddee2ea5f506fb134e45f59b0d8cb6d
abuzar-4a6c <abuzar_voetbal@msn.com>
2017-11-16 15:54:53 +0100

    slider tekst

--- includes/components/slider.php
+++ includes/components/slider.php
@@ -11,15 +11,44 @@
             <!-- Wrapper for slides -->
             <div class="carousel-inner">
                 <div class="item active">
-                    <div class="sliderHome" style="background: url(/images/discusvis1.jpg)" alt="fishing"></div>
+                    <div class="sliderHome" style="background: url(/images/discusvis1.jpg)" alt="fishing">
+                        <div class="sliderTekst">
+                            Now that there is the Tec-9, a crappy spray gun from South Miami.<br>
+                            This gun is advertised as the most popular gun in American crime.<br>
+                            Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime.
+                            Like they're actually proud of that shit.<br>
+                            <button class="lees-meer-btn pull-right " type="button" name="button">Lees meer</button>
+                        </div>
+                    </div>
                 </div>
 
                 <div class="item">
-                    <div class="sliderHome" style="background: url(/images/discusvis2.jpg)" alt="fishing"></div>
+                    <div class="sliderHome" style="background: url(/images/discusvis2.jpg)" alt="fishing">
+                        <div class="sliderTekst">
+                            Your bones don't break, mine do. That's clear.<br>
+                            Your cells react to bacteria and viruses differently than mine.<br>
+                            You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water.<br>
+                            We swallow it too fast, we choke. We get some in our lungs, we drown.<br>
+                            However unreal it may seem, we are connected, you and I.<br>
+                            We're on the same curve, just on opposite ends.<br>
+                            <button class="lees-meer-btn pull-right" type="button" name="button">Lees meer</button>
+                        </div>
+                    </div>
                 </div>
 
                 <div class="item">
-                    <div class="sliderHome" style="background: url(/images/discusvis3.jpg)" alt="vissen"></div>
+                    <div class="sliderHome" style="background: url(/images/discusvis3.jpg)" alt="vissen">
+                        <div class="sliderTekst">
+                            Do you see any Teletubbies in here?<br>
+                            Do you see a slender plastic tag clipped to my shirt with my name printed on it?<br>
+                            Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter
+                            that shakes when you put quarters in it? No?<br>
+                            Well, that's what you see at a toy store.<br>
+                            And you must think you're in a toy store,
+                            because you're here shopping for an infant named Jeb.<br>
+                            <button class="lees-meer-btn pull-right" type="button" name="button">Lees meer</button>
+                        </div>
+                    </div>
                 </div>
             </div>
 
