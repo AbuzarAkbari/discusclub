bf52e186203e4226f1cbc085cd1a15df5a922571
Melvin van Zutphen <melvinvanzutphen1995@outlook.com>
2017-11-16 16:16:32 +0100

    Required summernote on every page

--- pages/user/register.php
+++ pages/user/register.php
@@ -88,27 +88,13 @@
     <footer>
         <?php require_once("../../includes/components/footer.php") ; ?>
     </footer>
+    <!-- bootstrap script -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    <script type="text/javascript">
-    $( function() {
-        $( "#datepicker" ).datepicker({
-            changeMonth: true,
-            changeYear: true,
-            yearRange: "-90:+0",
-            defaultDate: '01/01/1980',
-            dateFormat: 'dd-mm-yy'
-        });
-    } );
-    $( function() {
-        $( "#datepicker" ).datepicker();
-        $( "#anim" ).on( "change", function() {
-            $( "#datepicker" ).datepicker( "option", "showAnim", $( this ).val() );
-        });
-    } );
-    </script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <?php require_once("../../includes/components/summernote.php"); ?>
     <script src="/js/password.js"></script>
 </body>
 
