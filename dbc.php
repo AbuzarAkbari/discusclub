cb2809c70c21a588a79132c9b4232db148d1d748
leon in 't Veld <leon3110l@gmail.com>
2017-11-20 13:45:04 +0100

    updated a query

--- dbc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    try {
-        $dbc = new PDO("mysql:host=127.0.0.1;dbname=forum", "root", "");
-        $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
-    }
-?>
