4f65c31fb18f355668391c682e96ef7137038a79
abuzar-4a6c <abuzar_voetbal@msn.com>
2017-11-20 11:32:26 +0100

    button ifisset

--- pages/contact.php
+++ pages/contact.php
@@ -47,7 +47,7 @@
                 <label for="subject">Bericht</label>
                 <textarea id="subject" name="bericht" class="form-control" placeholder="Uw bericht" style="height:200px; resize: none;" required></textarea>
                 <br>
-                <input type="submit" class="btn btn-primary" value="Verstuur">
+                <input type="submit" class="btn btn-primary" value="send">
               </form>
           </div>
         </div>
