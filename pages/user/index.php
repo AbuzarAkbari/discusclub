48504f0acb9b2f2fad05cba7b53dc2f170388280
Melvin van Zutphen <melvinvanzutphen1995@outlook.com>
2017-11-22 15:35:27 +0100

    fixed user profile permissions

--- pages/user/index.php
+++ pages/user/index.php
@@ -271,7 +271,7 @@ if($user_data == false){
                                     <?php
                                         $sql = "SELECT *, r.last_changed AS reply_last_changed, t.last_changed AS topic_last_changed, t.created_at AS topic_created_at, r.created_at AS reply_created_at, r.user_id AS reply_user_id, t.user_id AS topic_user_id, u.first_name AS reply_first_name, u.last_name AS reply_last_name, u2.first_name AS topic_first_name, u2.last_name AS topic_last_name, sc.name AS sub_cat_name, t.id AS topic_id FROM topic AS t JOIN topic_permission AS tp ON tp.topic_id = t.id LEFT JOIN reply AS r ON r.topic_id = t.id LEFT JOIN user AS u ON u.id = r.user_id LEFT JOIN user AS u2 ON u2.id = t.user_id LEFT JOIN sub_category AS sc ON sc.id = t.sub_category_id WHERE (t.user_id = :id OR r.user_id = :id) AND tp.role_id = :role_id AND t.deleted_at IS NULL ORDER BY reply_created_at DESC, topic_created_at DESC LIMIT 9";
                                         $result = $dbc->prepare($sql);
-                                        $result->execute([":id" => $user_data->id, ":role_id" => $user_data->role_id]);
+                                        $result->execute([":id" => $_SESSION['user']->id, ":role_id" => $_SESSION['user']->role_id]);
                                         $topics = $result->fetchAll(PDO::FETCH_OBJ);
 
                                     if(!empty($topics)) :
