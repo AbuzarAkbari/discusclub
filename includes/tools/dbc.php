3c7742ce173d1a0a783fafa4c06b57aae300de30
leon in 't Veld <leon3110l@gmail.com>
2017-11-23 10:50:17 +0100

    added db key stuff

--- includes/tools/dbc.php
+++ includes/tools/dbc.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $dbc = new PDO("mysql:host=127.0.0.1;dbname=discus", "discus", "R2mje6_G9lTlwhur", array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+    require_once("db.php");
     // $dbc = new PDO("mysql:host=127.0.0.1;dbname=forum", "root", "", array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
     $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
