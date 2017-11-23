c16c46970cb93775928dbec16efd61d193501cc4
leon in 't Veld <leon3110l@gmail.com>
2017-11-23 15:53:02 +0100

    fixed some stuff

--- pages/forum/topic.php
+++ pages/forum/topic.php
@@ -64,9 +64,9 @@ if(!empty($_POST['bevestig_topic'])) {
                         if($results2){
                             $results3 = [];
                             if((isset($_GET["pagina"]) && $_GET["pagina"] == 1) || !isset($_GET["pagina"])) {
-                                $sql = "SELECT *, topic.id, topic.last_changed AS topic_last_changed FROM topic JOIN user as u ON u.id = topic.user_id WHERE sub_category_id = :id AND state_id = 3 AND topic.deleted_at IS NULL ORDER BY topic.last_changed DESC LIMIT {$perPage} OFFSET {$offset}";
+                                $sql = "SELECT *, topic.id, topic.last_changed AS topic_last_changed FROM topic JOIN sub_category ON topic.sub_category_id = sub_category.id JOIN category_permission AS cp ON cp.category_id = sub_category.category_id JOIN sub_category_permission AS scp ON scp.sub_category_id = sub_category.id JOIN topic_permission AS tp ON tp.topic_id = topic.id JOIN user as u ON u.id = topic.user_id WHERE topic.sub_category_id = :id AND state_id = 3 AND topic.deleted_at IS NULL AND cp.role_id = :role_id AND scp.role_id = :role_id AND tp.role_id = :role_id ORDER BY topic.last_changed DESC LIMIT {$perPage} OFFSET {$offset}";
                                 $result = $dbc->prepare($sql);
-                                $result->execute([":id" => $_GET["id"]]);
+                                $result->execute([":id" => $_GET["id"], ":role_id" => $_SESSION['user']->role_id]);
                                 $results3 = $result->fetchAll(PDO::FETCH_ASSOC);
                             }
 
