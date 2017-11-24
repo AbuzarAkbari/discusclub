640e68ceec9856d316e538d96ee34baecbf1f66c
Wesley-4A6B <483509@edu.rocmn.nl>
2017-11-24 15:59:32 +0100

    Fixed rmScript on aquarium and forum

--- includes/tools/edit-topic.php
+++ includes/tools/edit-topic.php
@@ -3,8 +3,8 @@ require_once("../../includes/tools/security.php");
 if($logged_in) {
     if(isset($_POST['post_edit_topic'])) {
         $id = $_POST['subId'];
-        $title = $_POST['edit_topic_title'];
-        $content = $_POST['edit_topic_content'];
+        $title = rmScript($_POST['edit_topic_title']);
+        $content = rmScript($_POST['edit_topic_content']);
 
         $sql = "UPDATE topic SET title = :title, content = :content WHERE id = :id";
         $result = $dbc->prepare($sql);
