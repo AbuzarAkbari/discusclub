a992acc15e295f14c96d704863b100bf4345861f
Wesley-4A6B <483509@edu.rocmn.nl>
2017-11-24 16:19:33 +0100

    Fixed john

--- pages/about/bestuur/john.php
+++ pages/about/bestuur/john.php
@@ -39,17 +39,14 @@ require_once("../../../includes/components/nav.php");
                 <div class="panel-heading border-colors">John Maas- voorzitter DCH</div>
                 <div class="panel-body padding-padding space">
                     <p class="col-md-8">
-                        In 1975 ben ik begonnen met mijn eerste aquarium, destijds nog een stalen frame met ruiten in stopverf, wel RVS, dat was al heel wat. In de loop der jaren heb ik buiten discusvissen, heel wat bakken en soorten vissen gehad, tot in totaal misschien we een stuk of 20 thuis, overal waar ik een hoekje vrij had. Veel gekweekt, ook met van alle soorten vissen wel. En vanzelfsprekend met discusvissen. Ondertussen zijn we ruim 40 jaar verder, dus heb ik wel het nodige uitgeprobeerd en gedaan. En nog ben ik er dagelijks mee bezig.
-                    </p>
-                    <div class="col-md-4">
-                        <img src="/images/bestuur/john.jpg" class="John">
-                    </div>
-                    <p class="col-md-8">
-                        <br>
+                        In 1975 ben ik begonnen met mijn eerste aquarium, destijds nog een stalen frame met ruiten in stopverf, wel RVS, dat was al heel wat. In de loop der jaren heb ik buiten discusvissen, heel wat bakken en soorten vissen gehad, tot in totaal misschien we een stuk of 20 thuis, overal waar ik een hoekje vrij had. Veel gekweekt, ook met van alle soorten vissen wel. En vanzelfsprekend met discusvissen. Ondertussen zijn we ruim 40 jaar verder, dus heb ik wel het nodige uitgeprobeerd en gedaan. En nog ben ik er dagelijks mee bezig.<br>
                         In 2013, ben ik lid geworden van DCH, en in 2016, op de algemene leden vergadering, waar al geruime tijd van tevoren bestuursleden werden gevraagd, heb ik op dat moment besloten me aan te melden als bestuurslid, in eerste instantie als secretaris, later in 2017, toen Marcel van Hintum aftrad als voorzitter, heb ik de taak van voorzitter op me genomen.
                         Op diezelfde vergadering, stelden ook Jason en Rob zich niet meer verkiesbaar, zodat we met 4 man overbleven, inmiddels nog maar drie.
                         Ondanks dat proberen we er het beste van te maken en hoop ik, ook namens mijn mede bestuursleden dat we de club levend en actief kunnen houden.
                     </p>
+                    <div class="col-md-4">
+                        <img src="/images/bestuur/john.jpg" class="John">
+                    </div>
                 </div>
             </div>
         </div>
