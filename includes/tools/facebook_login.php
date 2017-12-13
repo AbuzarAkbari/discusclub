e805f6354374630cdff2c457ab3f657a81a54271
leon in 't Veld <leon3110l@gmail.com>
2017-12-13 16:18:56 +0100

    added profile pic from fb

--- includes/tools/facebook_login.php
+++ includes/tools/facebook_login.php
@@ -1,13 +1,12 @@
 <?php
 // facebook api call if data do shit
 require_once("./security.php");
-$data = file_get_contents("https://graph.facebook.com/me?fields=id,address,first_name,last_name,birthday,email,name&access_token=" . $_POST["accessToken"]);
+$data = file_get_contents("https://graph.facebook.com/me?fields=id,address,first_name,last_name,birthday,email,name,picture{url}&access_token=" . $_POST["accessToken"]);
 $data = json_decode($data);
 
 if(isset($data) && isset($data->id)) {
     loginOrRegister($data, $dbc);
 }
-
 
 function loginOrRegister($data, $dbc) {
     require("./const.php");
@@ -22,15 +21,30 @@ function loginOrRegister($data, $dbc) {
         echo '{"redirect": "' . (isset($_POST["redirect"]) ? $_POST["redirect"] : "/") . '"}';
 
     } else {
-        $sth = $dbc->prepare("INSERT INTO user(first_name, last_name, username, email, created_at, birthdate, password) VALUES
-                                              (:first_name, :last_name, :username, :email, NOW(), :birthdate, '')");
+
+        if($data->picture) {
+            $info = pathinfo($data->picture->data->url);
+            $ext = substr($info["extension"], 0, strpos($info["extension"], "?"));
+            file_put_contents("../../images/profile/" . $data->name . "." . $ext, file_get_contents($data->picture->data->url));
+
+            $sth = $dbc->prepare("INSERT INTO image(path) VALUES(:path)");
+            $sth->execute([":path" => "/profile/" . $data->name . "." . $ext]);
+
+        }
+
+        $sth = $dbc->prepare("INSERT INTO user(first_name, last_name, username, email, created_at, birthdate, password, profile_img) VALUES
+                                                (:first_name, :last_name, :username, :email, NOW(), :birthdate, '', :img_id)");
 
         $sth->execute([
             ":first_name" => $data->first_name,
             ":last_name" => $data->last_name,
             ":username" => $data->name,
             ":email" => $data->email,
-            ":birthdate" => date('Y-m-d', strtotime($data->birthday))]);
+            ":birthdate" => date('Y-m-d', strtotime($data->birthday)),
+            ":img_id" => $data->picture ? $dbc->lastInsertId() : 1
+        ]);
+
+
         loginOrRegister($data, $dbc);
     }
 }
